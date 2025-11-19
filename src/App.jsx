b18bd38cbc1f6b57b5c820ea5bf4a325e7e16542
Raz115 <razahmirza5@gmail.com>
2025-11-19 01:08:41 -0500

    setting up card grid and card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
 import { GameHeader } from "./components/GameHeader"
+import { Card } from "./components/Card";
+import { use, useEffect, useState } from "react";
+
+const cardValues = [
+"🐉","🦄","🐦‍🔥","🐻","🦖","🐟","🐥","🐸",
+"🐉","🦄","🐦‍🔥","🐻","🦖","🐟","🐥","🐸"
+]
 
 function App() {
+  
+  //console.log(cardValues);
+
+  const [cards, setCards] = useState([])
+
+  const intializeGame = () => {
+    const finalCards = cardValues.map((value, index) => ({
+        id: index,
+        value,
+        isFlipped: false,
+        isMatched: false
+      }));
+    
+    setCards(finalCards);
+
+  };
+
+  //console.log(finalCards);
+
+  useEffect(() => {
+    intializeGame();
+  }, []);
+  
   return  <div className = "app">
-    <GameHeader/>
+    <GameHeader score={12} moves={15}/>
+
+    <div className="cards-grid">
+        {cardValues.map((card, index) => (
+          <Card card={card}/>
+        ))}
+    </div>
+
   </div>
 }
 
